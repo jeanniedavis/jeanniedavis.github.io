fc8404ae641270aefc7871227526a0a8d41f53ef
jeanniedavis <51473352+jeanniedavis@users.noreply.github.com>
2020-02-23 01:55:40 -0800

    Updates

---
+++ precache-manifest.b8228025e16c1b878e60d7bbd23afef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb7f266a5c684041c890035747bf404",
+    "url": "/index.html"
+  },
+  {
+    "revision": "603dd8e1a535204ea1e0",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "cbf7381bbe666877fb11",
+    "url": "/static/js/2.f5d2214b.chunk.js"
+  },
+  {
+    "revision": "6e498969aafde287c0e2e1a721426fda",
+    "url": "/static/js/2.f5d2214b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "603dd8e1a535204ea1e0",
+    "url": "/static/js/main.06a87923.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
